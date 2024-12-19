7b285bbf99459bc06d21df3fc3ba2dfd11a4a938
Yannic <r4zzeryt@gmail.com>
2024-12-19 21:46:52 +0100

    improved content-security-policy header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent {
     // add Content-Security-Policy to header
     const meta = document.createElement('meta');
     meta.httpEquiv = 'Content-Security-Policy';
-    meta.content = "default-src 'self'; script-src 'self' " + config.api_url + "; object-src 'none'; style-src 'self' 'unsafe-inline'; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://cdn.discordapp.com; connect-src 'self' " + config.api_url + " https://discord.com;";
+    meta.content = "default-src 'self'; script-src 'self' " + config.api_url + "; object-src 'none'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://cdn.discordapp.com; connect-src 'self' " + config.api_url + " https://discord.com;";
     document.getElementsByTagName('head')[0].appendChild(meta);
   }
 }
