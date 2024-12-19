e9478b5e01490bd394a6941403702b91bd65a2f7
Yannic Drews <49283907+RazzerDE@users.noreply.github.com>
2024-12-19 19:22:32 +0100

    Update config.ts

--- src/environments/config.ts
+++ src/environments/config.ts
@@ -1,5 +1,6 @@
 export const config = {
-  api_url: 'http://localhost:4200',  // used for general api calls
-  redirect_url: 'http://localhost:4200/authenticate',  // used for the discord oauth2
+  domain: 'http://localhost:4200',
+  api_url: 'http://localhost:8081',  // used for general api calls
+  redirect_url: 'http://localhost:4200/dashboard',  // used for the discord oauth2
   client_id: 'YOUR_DISCORD_CLIENT_ID'  // used for the discord oauth2
 }
