dd8095cf077422657fde5f63148578619bc667a3
Yannic <r4zzeryt@gmail.com>
2024-12-23 18:10:21 +0100

    implemented experimental sidebar with finished header content

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,21 +1,42 @@
 import {Component} from '@angular/core';
 import {AuthService} from "../../services/auth/auth.service";
 import {DataHolderService} from "../../services/data/data-holder.service";
+import {NgOptimizedImage} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    RouterLink
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
 
-  constructor(private authService: AuthService, private dataService: DataHolderService) {
-    this.dataService.isLoading = true;
+  navigation = [
+    {
+      category: "Analytics",
+      pages: [
+        { title: "Dashboard", icon: "" },
+        { title: "Statistics", icon: "" }
+      ]
+    },
+    {
+      category: "Dashboard",
+      pages: [
+        { title: "Dashboard", icon: "" },
+        { title: "Statistics", icon: "" }
+      ]
+    }
+  ];
+
+  constructor(protected authService: AuthService, private dataService: DataHolderService) {
+    this.dataService.isLoading = false;
 
     this.authService.discordLogin();
   }
-
 
 }
