6c8cad7dc41e4fc2602a88efd3b3ede8acc57190
Yannic <r4zzeryt@gmail.com>
2024-12-30 21:51:55 +0100

    added language support for dashboard page design

--- src/app/pages/dashboard/types/Tasks.ts
+++ src/app/pages/dashboard/types/Tasks.ts
@@ -15,31 +15,31 @@ export const tasks: Tasks[] = [
   {
     position: 1,
     finished: true,
-    title: 'Support-System eingerichtet & nutzbar für neue Tickets',
+    title: 'SECTION_BOT_SETUP_TASK_SUPPORT',
     subtasks: [
-      { name: 'Discord Forumkanal erstellt & eingerichtet', finished: true, redirect_url: '/support/setup' },
-      { name: 'Server-Team festgelegt für eine verbesserte Verwendung', finished: true, redirect_url: '/teamlist' },
-      { name: 'Support-Themen erstellt die User bei Tickets auswählen können', finished: true, redirect_url: '/support/themes' },
-      { name: 'Ticket-Erwähnungen festlegen, um sofort informiert zu werden', finished: true, redirect_url: '/support/themes' },
+      { name: 'SECTION_BOT_SETUP_TASK_SUPPORT_1', finished: true, redirect_url: '/support/setup' },
+      { name: 'SECTION_BOT_SETUP_TASK_SUPPORT_2', finished: true, redirect_url: '/teamlist' },
+      { name: 'SECTION_BOT_SETUP_TASK_SUPPORT_3', finished: true, redirect_url: '/support/themes' },
+      { name: 'SECTION_BOT_SETUP_TASK_SUPPORT_4', finished: true, redirect_url: '/support/themes' },
     ]
   },
   {
     position: 2,
     finished: null,
-    title: 'Backup-Schutz & Sicherheitsmaßnahmen aktiv',
+    title: 'SECTION_BOT_SETUP_TASK_SECURITY',
     subtasks: [
-      { name: 'Log-System eingerichtet um über wichtige Ereignisse informiert zu werden', finished: false, redirect_url: '/security/automod' },
-      { name: 'AutoModeration\'s Regeln für mehr Chatkontrolle eingerichtet', finished: true, redirect_url: '/security/automod' },
-      { name: 'Entsperrungs-Methode festlegen, damit gesperrte User sich entschuldigen können', finished: false, redirect_url: '/security/shield' },
-      { name: 'Nukeschutz aktiv & ein Backup wurde erstellt', finished: true, redirect_url: '/security/backups' }
+      { name: 'SECTION_BOT_SETUP_TASK_SECURITY_1', finished: false, redirect_url: '/security/automod' },
+      { name: 'SECTION_BOT_SETUP_TASK_SECURITY_2', finished: true, redirect_url: '/security/automod' },
+      { name: 'SECTION_BOT_SETUP_TASK_SECURITY_3', finished: false, redirect_url: '/security/shield' },
+      { name: 'SECTION_BOT_SETUP_TASK_SECURITY_4', finished: true, redirect_url: '/security/backups' }
     ],
   },
   {
     position: 3,
     finished: false,
-    title: 'Global-Chat eingerichtet, um serverweit zu chatten',
+    title: 'SECTION_BOT_SETUP_TASK_GLOBAL',
     subtasks: [
-      { name: 'Kanal für das globale Chatten festgelegt', finished: false, redirect_url: '/misc/global-chat' }
+      { name: 'SECTION_BOT_SETUP_TASK_GLOBAL_1', finished: false, redirect_url: '/misc/global-chat' }
     ],
   },
 ];
