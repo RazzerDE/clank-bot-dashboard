97e0db5d0b16889709d69ac3fd84a09f78b7d184
Yannic <r4zzeryt@gmail.com>
2025-01-17 21:29:34 +0100

    added sidebar tags for wishlist with search input & suggest button

--- src/app/pages/dashboard/wishlist/wishlist.component.ts
+++ src/app/pages/dashboard/wishlist/wishlist.component.ts
@@ -5,6 +5,16 @@ import {ReactiveFormsModule} from "@angular/forms";
 import {SidebarComponent} from "../../../structure/sidebar/sidebar.component";
 import {NgClass} from "@angular/common";
 import {TranslatePipe} from "@ngx-translate/core";
+import {Tag} from "../../../services/types/navigation/WishlistTags";
+import {faLifeRing} from "@fortawesome/free-regular-svg-icons";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {
+  faBars,
+  faGift,
+  faLightbulb,
+  faSearch,
+  faShieldHalved, faTools, IconDefinition
+} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-wishlist',
@@ -14,16 +24,27 @@ import {TranslatePipe} from "@ngx-translate/core";
     ReactiveFormsModule,
     SidebarComponent,
     NgClass,
-    TranslatePipe
+    TranslatePipe,
+    FaIconComponent
   ],
   templateUrl: './wishlist.component.html',
   styleUrl: './wishlist.component.scss'
 })
 export class WishlistComponent {
+  protected isDev: boolean = false;
+  protected readonly faSearch: IconDefinition = faSearch;
+  protected readonly faLightbulb: IconDefinition = faLightbulb;
+
+  protected tags: Tag[] = [
+    { name: "WISHLIST_TAG_FEATURES", icon: faBars, isActive: true },
+    { name: 'Support-Tool', icon: faLifeRing, isActive: false },
+    { name: 'Security-System', icon: faShieldHalved, isActive: false },
+    { name: 'WISHLIST_TAG_GIVEAWAYS', icon: faGift, isActive: false },
+    { name: 'WISHLIST_TAG_MODULES', icon: faTools, isActive: false }
+  ];
 
   constructor(protected dataService: DataHolderService) {
     this.dataService.isLoading = false;
     this.dataService.hideGuildSidebar = false;
   }
-
 }
---
+++ src/app/services/types/navigation/WishlistTags.ts
@@ -0,0 +1,7 @@
+import {IconDefinition} from "@fortawesome/free-solid-svg-icons";
+
+export interface Tag {
+  name: string;
+  icon: IconDefinition;
+  isActive: boolean;
+}
