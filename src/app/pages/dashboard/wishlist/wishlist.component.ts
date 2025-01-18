c5e0885bdb5d6584fca2516f074f9b21ec063a37
Yannic <r4zzeryt@gmail.com>
2025-01-18 20:34:18 +0100

    refactored code for wishlist items

--- src/app/pages/dashboard/wishlist/wishlist.component.ts
+++ src/app/pages/dashboard/wishlist/wishlist.component.ts
@@ -1,19 +1,18 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, HostListener, ViewChild} from '@angular/core';
 import {DataHolderService} from "../../../services/data/data-holder.service";
 import {HeaderComponent} from "../../../structure/header/header.component";
 import {ReactiveFormsModule} from "@angular/forms";
 import {SidebarComponent} from "../../../structure/sidebar/sidebar.component";
-import {NgClass} from "@angular/common";
+import {NgClass, NgOptimizedImage} from "@angular/common";
 import {TranslatePipe} from "@ngx-translate/core";
-import {Tag} from "../../../services/types/navigation/WishlistTags";
-import {faLifeRing} from "@fortawesome/free-regular-svg-icons";
+import {Feature, Tag} from "../../../services/types/navigation/WishlistTags";
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 import {
-  faBars,
-  faGift, faHashtag, faInbox,
+  faClock,
+  faHashtag, faInbox,
   faLightbulb,
-  faSearch,
-  faShieldHalved, faTools, IconDefinition
+  faSearch, faThumbsUp,
+  IconDefinition
 } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
@@ -25,28 +24,57 @@ import {
     SidebarComponent,
     NgClass,
     TranslatePipe,
-    FaIconComponent
+    FaIconComponent,
+    NgOptimizedImage
   ],
   templateUrl: './wishlist.component.html',
   styleUrl: './wishlist.component.scss'
 })
-export class WishlistComponent {
+export class WishlistComponent implements AfterViewInit {
   protected isDev: boolean = false;
   protected readonly faSearch: IconDefinition = faSearch;
   protected readonly faLightbulb: IconDefinition = faLightbulb;
   protected readonly faHashtag: IconDefinition = faHashtag;
   protected readonly faInbox: IconDefinition = faInbox;
+  protected readonly faThumbsUp: IconDefinition = faThumbsUp;
+  protected readonly faClock: IconDefinition = faClock;
+
+  @ViewChild('Divider') protected divider!: ElementRef<HTMLDivElement>
+  @ViewChild('WishlistContainer') protected wishlistContainer!: ElementRef<HTMLDivElement>
 
   protected tags: Tag[] = [
-    { name: "WISHLIST_TAG_FEATURES", icon: faBars, isActive: true },
-    { name: 'Support-Tool', icon: faLifeRing, isActive: false },
-    { name: 'Security-System', icon: faShieldHalved, isActive: false },
-    { name: 'WISHLIST_TAG_GIVEAWAYS', icon: faGift, isActive: false },
-    { name: 'WISHLIST_TAG_MODULES', icon: faTools, isActive: false }
+    { id: 1, name: "WISHLIST_TAG_FEATURES", isActive: true },
+    { id: 2, name: 'Support-Tool', isActive: false },
+    { id: 3, name: 'Security-System', isActive: false },
+    { id: 4, name: 'WISHLIST_TAG_GIVEAWAYS', isActive: false },
+    { id: 5, name: 'WISHLIST_TAG_MODULES', isActive: false }
   ];
+
+  protected feature_list: Feature[] = [
+    { name: 'Modernes Levelsystem', icon_url: "assets/img/icons/utility/star.png", tag_id: 5, votes: 10, dislikes: 4,
+      created_at: '23.01.2025', desc: "› User sollen XP erhalten und Level aufsteigen, in dem sie sich auf dem Server aktiv beteiligen." },
+    { name: 'Temp-Voice-System', icon_url: "assets/img/icons/utility/sound.png", tag_id: 5, votes: 3, dislikes: 0,
+      created_at: '19.01.2025', desc: "› User sollen temporäre Voice-Channels erstellen können, die automatisch gelöscht werden." },
+    { name: 'Live-Benachrichtigungen', icon_url: "assets/img/icons/utility/live.png", tag_id: 5, votes: 3, dislikes: 6,
+      created_at: '19.01.2025', desc: "› Admins sollen YouTube/Twitch-Kanäle festlegen können, wobei der Bot Live-Infos rausschickt." },
+    { name: 'Willkommens-Nachrichten', icon_url: "assets/img/icons/utility/wave.png", tag_id: 5, votes: 5, dislikes: 21,
+      created_at: '19.01.2025', desc: "› Server sollten stark personalisierte Willkommens-Nachrichten & Join-Rollen festlegen können." },
+  ]
 
   constructor(protected dataService: DataHolderService) {
     this.dataService.isLoading = false;
     this.dataService.hideGuildSidebar = false;
   }
+
+  ngAfterViewInit(): void {
+    this.setResponsiveHeight();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  @HostListener('document:fullscreenchange', ['$event'])
+  protected setResponsiveHeight(): void {
+    this.wishlistContainer.nativeElement.style.height = `${this.divider.nativeElement.offsetHeight}px`;
+  }
+
+
 }
--- src/app/services/types/navigation/WishlistTags.ts
+++ src/app/services/types/navigation/WishlistTags.ts
@@ -1,7 +1,15 @@
-import {IconDefinition} from "@fortawesome/free-solid-svg-icons";
-
 export interface Tag {
+  id: number;
   name: string;
-  icon: IconDefinition;
   isActive: boolean;
+}
+
+export interface Feature {
+  name: string;
+  icon_url: string;
+  tag_id: number;
+  votes: number;
+  dislikes: number;
+  created_at: string;
+  desc: string;
 }
