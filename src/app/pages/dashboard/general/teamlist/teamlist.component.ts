abd10b4ade9a6b710ff33181b8457bb5f19299aa
Yannic <r4zzeryt@gmail.com>
2025-02-05 20:39:53 +0100

    added placeholder table with Support-Level filter & add role modal

--- src/app/pages/dashboard/general/teamlist/teamlist.component.ts
+++ src/app/pages/dashboard/general/teamlist/teamlist.component.ts
@@ -3,14 +3,19 @@ import {DataHolderService} from "../../../../services/data/data-holder.service";
 import {TranslatePipe} from "@ngx-translate/core";
 import {PageThumbComponent} from "../../../../structure/util/page-thumb/page-thumb.component";
 import {DashboardLayoutComponent} from "../../../../structure/dashboard-layout/dashboard-layout.component";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {faFilter, faSearch, faXmark} from "@fortawesome/free-solid-svg-icons";
+import {faPlus} from "@fortawesome/free-solid-svg-icons/faPlus";
+import {faChevronDown} from "@fortawesome/free-solid-svg-icons/faChevronDown";
 
 @Component({
   selector: 'app-teamlist',
-  imports: [
-    TranslatePipe,
-    PageThumbComponent,
-    DashboardLayoutComponent
-  ],
+    imports: [
+        TranslatePipe,
+        PageThumbComponent,
+        DashboardLayoutComponent,
+        FaIconComponent
+    ],
   templateUrl: './teamlist.component.html',
   styleUrl: './teamlist.component.scss'
 })
@@ -20,4 +25,9 @@ export class TeamlistComponent {
     this.dataService.isLoading = false;
   }
 
+  protected readonly faSearch = faSearch;
+  protected readonly faPlus = faPlus;
+  protected readonly faFilter = faFilter;
+  protected readonly faChevronDown = faChevronDown;
+  protected readonly faXmark = faXmark;
 }
