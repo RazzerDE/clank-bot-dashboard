f8f742a58de5d1500c3851d885936bb9bc2586a3
Yannic <r4zzeryt@gmail.com>
2025-03-25 14:50:19 +0100

    added design of status box for Support-Setup page

--- src/app/pages/dashboard/support/module-setup/module-setup.component.ts
+++ src/app/pages/dashboard/support/module-setup/module-setup.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import {DashboardLayoutComponent} from "../../../../structure/dashboard-layout/dashboard-layout.component";
 import {DataHolderService} from "../../../../services/data/data-holder.service";
+import {NgClass, NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-module-setup',
-    imports: [
-        DashboardLayoutComponent
-    ],
+  imports: [
+    DashboardLayoutComponent,
+    NgOptimizedImage,
+    NgClass
+  ],
   templateUrl: './module-setup.component.html',
   styleUrl: './module-setup.component.scss'
 })
 export class ModuleSetupComponent {
+  // 0 = Not started, 1 = In progress, 2 = Completed (TODO: Real data)
+  protected moduleStatus: 0 | 1 | 2 = 0;
 
   constructor(private dataService: DataHolderService) {
     document.title = 'Support Setup ~ Clank Discord-Bot';
