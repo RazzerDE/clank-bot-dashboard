b6e08b9fc00849760eafafa7424aed8dd6b600f8
Yannic <r4zzeryt@gmail.com>
2025-03-29 15:04:47 +0100

    implemented design for forum channel picker inside of module setup page

--- src/app/pages/dashboard/support/module-setup/module-setup.component.ts
+++ src/app/pages/dashboard/support/module-setup/module-setup.component.ts
@@ -15,12 +15,32 @@ import {NgClass, NgOptimizedImage} from "@angular/common";
 })
 export class ModuleSetupComponent {
   // 0 = Not started, 1 = In progress, 2 = Completed (TODO: Real data)
-  protected moduleStatus: 0 | 1 | 2 = 0;
+  protected moduleStatus: 0 | 1 | 2 = 2;
   protected currentStep: 1 | 2 | 3 = 1; // TODO
+  protected channelItems: { id: number, name: string }[] = [
+    { id: 123456789, name: 'test' },
+    { id: 456789901, name: 'test' },
+    { id: 454565465, name: 'test' },
+    { id: 122432345, name: 'test' },
+    { id: 656745464, name: 'test' },
+    { id: 876856756, name: 'test' },
+    { id: 576784567, name: 'test' },
+    { id: 435546456, name: 'test' },
+    { id: 324435567, name: 'test' },
+    { id: 324436658, name: 'test' },
+    { id: 435767967, name: 'test' },
+    { id: 859465564, name: 'test' },
+    { id: 406587546, name: 'test' },
+  ]; // TODO: Real data; sort by id and alphabetically
+  protected selectedChannelId: number | null = null;
 
   constructor(private dataService: DataHolderService) {
     document.title = 'Support Setup ~ Clank Discord-Bot';
     this.dataService.isLoading = false;
+  }
+
+  protected selectChannel(id: number): void {
+    this.selectedChannelId = this.selectedChannelId === id ? null : id;
   }
 
 }
