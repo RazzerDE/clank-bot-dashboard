e87e9e3b285da63ccc25a0d6b2a10138073980b9
Yannic <r4zzeryt@gmail.com>
2025-04-13 12:13:43 +0200

    implemented page-thumb for "Support Themes" page

--- src/app/pages/dashboard/support/support-themes/support-themes.component.ts
+++ src/app/pages/dashboard/support/support-themes/support-themes.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import {DashboardLayoutComponent} from "../../../../structure/dashboard-layout/dashboard-layout.component";
+import {PageThumbComponent} from "../../../../structure/util/page-thumb/page-thumb.component";
+import {TranslatePipe} from "@ngx-translate/core";
+import {DataHolderService} from "../../../../services/data/data-holder.service";
 
 @Component({
   selector: 'app-support-themes',
-    imports: [
-        DashboardLayoutComponent
-    ],
+  imports: [
+    DashboardLayoutComponent,
+    PageThumbComponent,
+    TranslatePipe
+  ],
   templateUrl: './support-themes.component.html',
   styleUrl: './support-themes.component.scss'
 })
 export class SupportThemesComponent {
+
+  constructor(public dataService: DataHolderService) {
+    this.dataService.isLoading = false;
+  }
 
 }
