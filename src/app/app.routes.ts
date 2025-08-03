41f7af8f3baebb3f855ff78a4aa3332a4a15e76d
Yannic <r4zzeryt@gmail.com>
2025-08-03 17:04:12 +0200

    fixed infinity loop for auth requests on dashboard pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {LandingPageComponent} from "./pages/landing-page/landing-page.component";
 import {SimpleErrorComponent} from "./pages/dashboard/errors/simple-error.component";
+import {AuthGuard} from "./guards/auth.guard";
 
 export const routes: Routes = [
   // General pages
@@ -8,7 +9,7 @@ export const routes: Routes = [
   { path: 'de', component: LandingPageComponent },
 
   // Dashboard pages (lazy loaded)
-  { path: 'dashboard',
+  { path: 'dashboard', canActivate: [AuthGuard],
     loadChildren: () => import('../app/pages/dashboard/dashboard.routes').then(m => m.dashboardRoutes),
   },
 
