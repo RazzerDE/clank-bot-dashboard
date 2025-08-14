750b0950b3ce086a8be66d2ae2f5bed90e3edb44
Yannic <r4zzeryt@gmail.com>
2025-08-14 13:10:49 +0200

    added bot invite button if guild has no clank

--- src/app/pages/dashboard/errors/simple-error.component.ts
+++ src/app/pages/dashboard/errors/simple-error.component.ts
@@ -4,7 +4,7 @@ import {Router, RouterLink} from "@angular/router";
 import {NgClass, NgOptimizedImage} from "@angular/common";
 import {TranslatePipe, TranslateService} from "@ngx-translate/core";
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
-import {faHome, IconDefinition} from "@fortawesome/free-solid-svg-icons";
+import {faHome, faLink, IconDefinition} from "@fortawesome/free-solid-svg-icons";
 import {faDiscord} from "@fortawesome/free-brands-svg-icons";
 import {LangSwitchButtonComponent} from "../../../structure/util/lang-switch-button/lang-switch-button.component";
 import {ThemeSwitchButtonComponent} from "../../../structure/util/theme-switch-button/theme-switch-button.component";
@@ -27,7 +27,12 @@ export class SimpleErrorComponent implements AfterViewInit {
 
   protected readonly faHome: IconDefinition = faHome;
   protected readonly faDiscord: IconDefinition = faDiscord;
+  protected readonly faLink: IconDefinition = faLink;
   protected readonly localStorage: Storage = localStorage;
+
+  protected invite_url: string = 'https://discord.com/oauth2/authorize?client_id=775415193760169995 ' +
+    '&permissions=10430293995255&integration_type=0&scope=bot+applications.commands' +
+    `&guild_id=${this.dataService.active_guild?.id}&disable_guild_select=true`
 
   constructor(protected dataService: DataHolderService, private translate: TranslateService, protected router: Router) {
     this.dataService.isLoading = true;
@@ -41,6 +46,9 @@ export class SimpleErrorComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     this.translate.onLangChange.subscribe((): void => {
       document.title = this.translate.instant('ERROR_PAGE_TITLE');
+      this.invite_url = 'https://discord.com/oauth2/authorize?client_id=775415193760169995 ' +
+        '&permissions=10430293995255&integration_type=0&scope=bot+applications.commands' +
+        `&guild_id=${this.dataService.active_guild?.id}&disable_guild_select=true`
     });
 
     if (this.router.url != '/errors/simple') {  // custom 404 error page
